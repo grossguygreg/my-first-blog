d2ca49b8e8aac5c58664a7e8d313f59fc6d3e7c8
grossguygreg <hlundstedt77@gmail.com>
2016-09-29 19:31:18 +0200

    Besökare kan skriva kommenterer, inloggning från postlistan

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from .forms import CommentForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -25,3 +26,16 @@ def post_new(request):
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
+
+def add_comment_to_post(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method =="POST":
+		form = CommentForm(request.POST)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect ('blog.views.post_detail', pk=post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'blog/add_comment_to_post.html', {'form': form})
